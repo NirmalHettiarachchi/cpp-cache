636c168b59530c1edf40810a4fec585356b53a06
Nirmal Hettiarachchi <nirmalhettiarachchi5@gmail.com>
2026-05-03 16:00:00 +0530

    feat(slru): add protected_ratio constructor parameter with default 0.7

--- include/cache.hpp
+++ include/cache.hpp
@@ -1551,4 +1551,4 @@ auto make_memoized(std::size_t capacity, Fn&& fn) {
 
 #endif  // CPP_CACHE_CACHE_HPP_
 
-// history marker: slru policy landed
+// history marker: slru protected ratio constructor
